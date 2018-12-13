0d21110ebdb4b6b45425633ab5d8562a67ad7fb5
Felipe Medeiros <felipe.meddeiros@hotmail.com>
2018-12-13 18:51:01 -0200

    Adequação de dados recebidos

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,11 @@
-import { handleStatus } from './utils/promise-helpers.js';
-
+import { log } from './utils/promise-helpers.js';
+import './utils/array-helpers.js';
+import { notasService as service } from './nota/service.js'; 
 
 document
 .querySelector('#myButton')
 .onclick = () => 
-    fetch('http://localhost:3000/notasx')
-    .then(handleStatus)
-    .then(notas => console.log(notas))
+    service
+    .sumItems('2143')
+    .then(console.log)
     .catch(console.log);
