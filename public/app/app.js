53ff5146e171e025f15d9b111ccafef9e93ca153
Felipe Medeiros <felipe.meddeiros@hotmail.com>
2019-01-09 18:59:13 -0200

    take until and debounce

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,20 @@
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js'; 
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
-document
-.querySelector('#myButton')
-.onclick = () => 
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+const action = operations(() => 
     service
     .sumItems('2143')
     .then(console.log)
-    .catch(console.log);
+    .catch(console.log)
+);
+
+document
+.querySelector('#myButton')
+.onclick = action;
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -4,3 +4,14 @@ export const partialize = (fn, ...params) =>
 export const pipe = (...fns) => value => 
   fns.reduce((previousValue, fn) => 
       fn(previousValue), value);
+
+export const takeUntil = (times, fn) => 
+	() => times-- > 0 && fn();
+
+export const debounceTime = (milliseconds, fn) => {
+    let timer = 0;
+    return () => {
+        clearTimeout(timer);
+        timer = setTimeout(fn, milliseconds);
+    };
+};
