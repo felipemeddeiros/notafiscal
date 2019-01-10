644452ab57f8e7e2c791ab4ecd31147a7ac5a787
Felipe Medeiros <felipe.meddeiros@hotmail.com>
2019-01-10 19:05:09 -0200

    promise race, retry and delay

--- public/app/app.js
+++ public/app/app.js
@@ -1,16 +1,15 @@
-import { log } from './utils/promise-helpers.js';
+import { log, timeoutPromise, retry} from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js'; 
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
 const operations = pipe(
     partialize(takeUntil, 3),
-    partialize(debounceTime, 500)
+    partialize(debounceTime, 500), 
 );
 
 const action = operations(() => 
-    service
-    .sumItems('2143')
+    retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
     .then(console.log)
     .catch(console.log)
 );
