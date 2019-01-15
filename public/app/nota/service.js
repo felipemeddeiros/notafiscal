fb3ff063b5f0d39d515b325c7154d362f7e6a671
Felipe Medeiros <felipe.meddeiros@hotmail.com>
2019-01-15 19:25:42 -0200

    manada and class maybe

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,32 +1,40 @@
 import { handleStatus } from '../utils/promise-helpers.js';
 import { partialize, pipe } from '../utils/operators.js';
+import { Maybe } from '../utils/maybe.js';
 
-const API = `http://localhost:3000/notas`;
+const API = 'http://localhost:3000/notas';
 
-const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
-const filterItemsByCode = (code, items) => items.filter(item => item.codigo === code);
-const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+// adequou cada função para receber o tipo monádico
+const getItemsFromNotas = notasM => notasM.map(notas => notas.$flatMap(nota => nota.itens));
+const filterItemsByCode = (code, itemsM) => itemsM.map(items => items.filter(item => item.codigo == code));
+const sumItemsValue = itemsM => itemsM.map(items => items.reduce((total, item) => total + item.valor, 0));
 
 export const notasService = {
 
     listAll() {
+
         return fetch(API)
-        .then(handleStatus)
-        .catch(err => {
-            console.log(err);
-            return Promise.reject('Não foi possível obter as notas fiscais');
-        });
+            .then(handleStatus)
+            // retorna um `Maybe`
+            .then(notas => Maybe.of(notas))
+            .catch(err => {
+                console.log(err);
+                return Promise.reject('Não foi possível obter as notas fiscais');
+            });
     },
 
     sumItems(code) {
+        const filterItems = partialize(filterItemsByCode, code);
 
-        // realizando a composição
         const sumItems = pipe(
-            getItemsFromNotas,
-            partialize(filterItemsByCode, '2143'), 
-            sumItemsValue, 
+            getItemsFromNotas, 
+            filterItems, 
+            sumItemsValue
         );
 
-        return this.listAll().then(sumItems);
+        return this.listAll()
+            .then(sumItems)
+            // obtendo o valor da mônada 
+            .then(result => result.getOrElse(0))
     }
 };
